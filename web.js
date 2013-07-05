d21f4ec13f6aa7d7535ce8a5c1a52d343b6625d0
karampok@aws <karampok@gmail.com>
2013-07-05 08:54:38 +0000

    Modified web.js

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
+var index_file = "index.html";
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var content=fs.readFileSync(index_file);
+  response.send(content.toString());
 });
 
 var port = process.env.PORT || 5000;
